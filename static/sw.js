e61e42315aec2c9a06343556e543f1a27c721bca
Sharv619 <hlade03@gmail.com>
2026-04-14 12:15:54 +1000

    feat: add PWA support - manifest, service worker, icons, mobile meta tags

---
+++ static/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'backpocket-v1';
+const ASSETS = [
+  '/static/index.html',
+  '/static/manifest.json',
+  '/static/app.js',
+  '/static/style.css',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetched = fetch(event.request)
+        .then((response) => {
+          if (response.ok) {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          }
+          return response;
+        })
+        .catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
